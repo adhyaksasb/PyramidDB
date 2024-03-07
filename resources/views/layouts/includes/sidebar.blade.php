5f1fd96bb311b274ca030aeef9d0396f7db69769
adhyaksasb <adhyaksasb@gmail.com>
2024-03-07 19:43:01 +0700

    Deploy to Vercel

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="fixed h-screen left-0 top-0 hidden flex-col gap-y-4 items-center py-5 w-24 bg-dark-900 md:flex">
     <a wire:navigate href="{{ route('home') }}">
-        <img src="{{ asset('storage/logo-mini.svg') }}" alt="" class="px-2 w-[4.25rem] h-[4.25rem]">
+        <img src="{{ asset('img/logo-mini.svg') }}" alt="" class="px-2 w-[4.25rem] h-[4.25rem]">
     </a>
     <div class="flex flex-col gap-y-4 items-end self-end">
         <x-sidebar-button href="{{ route('home') }}" name="Home" src='HomeIconBlack.png' :active="request()->routeIs('home')" />
